2adc0184747ace15464437bf3e58be14a7685c14
itsasunday <sunday.kfk@gmail.com>
2024-06-21 21:16:50 +0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function showResponse(response) {
+document.getElementById('response').style.display = 'block';
+document.getElementById('response').innerHTML = `<p class="message">Terima kasih telah memberi kesempatan. Aku akan berusaha sebaik mungkin untuk membuat segalanya lebih baik.</p>`;
+}
